a0fe215631b91e2295df6b339544fe4de9da1491
pvanhout <pierre.vanhoutryve@amd.com>
2026-03-30 11:55:36 +0200

    [AMDGPU] Make globally-addressable-scratch opt-in
    This feature is meant to be opt-in for more advanced users, not default-enabled.
    It may reduce performance otherwise as we can't assume private AS is thread-local
    when it is enabled.
    
    - Add `HasGloballyAddressableScratchSupport` feature to check if a target's scratch
      addressing is changed due to support for globally addressable scratch.
    - Use `EnableGloballyAddressableScratch` to check whether the user opted into
      globally addressable scratch. This affects whether to lower scratch atomics as flat,
      and in the future will affect whether NV=1 can be set on scratch accesses.

--- llvm/lib/Target/AMDGPU/GCNSubtarget.h
+++ llvm/lib/Target/AMDGPU/GCNSubtarget.h
@@ -1036,6 +1036,11 @@ public:
   bool requiresWaitOnWorkgroupReleaseFence() const {
     return getGeneration() >= GFX10 || isTgSplitEnabled();
   }
+
+  bool isGloballyAddressableScratchEnabled() const {
+    return HasGloballyAddressableScratchSupport &&
+           EnableGloballyAddressableScratch;
+  }
 };
 
 class GCNUserSGPRUsageInfo {
